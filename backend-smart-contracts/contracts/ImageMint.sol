8b352fd4ed3c86616e31c678d1db3411d5399887
Bhimgouda <satvikp508@gmail.com>
2023-05-18 16:00:48 +0530

    basic contract

--- backend-smart-contracts/contracts/ImageMint.sol
+++ backend-smart-contracts/contracts/ImageMint.sol
@@ -1,4 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract ImageMint {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Testing is ERC721, Ownable {
+    // NFT contract Metadata
+    string private constant NAME = "Testing";
+    string private constant SYMBOL = "TST";
+
+    // NFT state variables
+    uint256 private s_tokenCounter;
+    string[] private s_tokenUris = new string[](0);
+
+    constructor() ERC721(NAME, SYMBOL) {}
+
+    function requestNft(
+        address nftOwner,
+        string memory tokenUri
+    ) external onlyOwner {
+        uint256 tokenId = s_tokenCounter;
+        _safeMint(nftOwner, tokenId);
+        s_tokenUris.push(tokenUri);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint tokenId
+    ) public view override returns (string memory) {
+        return s_tokenUris[tokenId];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
--- backend-smart-contracts/deploy/01-deploy-image-mint.js
+++ backend-smart-contracts/deploy/01-deploy-image-mint.js
@@ -7,6 +7,18 @@ const {verify} = require("../utils/verify")
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy, log, get} = deployments
     const {deployer} = await getNamedAccounts()
-    const {name: networkName} = network;
 
+    const args = []
+
+    const ImageMint = await deploy("Testing", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying.....")
+        await verify(ImageMint.address, args)
+    }
 }
